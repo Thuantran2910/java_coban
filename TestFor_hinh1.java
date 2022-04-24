8a8dba2e42db9d84b1ac8245b9b250ae0ed02c43
Thuantran2910 <99887435+Thuantran2910@users.noreply.github.com>
2022-04-24 23:36:38 +0700

    Add files via upload

---
+++ TestFor_hinh1.java
@@ -0,0 +1,18 @@
+import java.net.NoRouteToHostException;
+
+public class TestFor_hinh1 {
+    public static void hinh1(int h) {
+        for(int i = 0;i<h;i++){
+            for(int j=i;j<h;j++){
+                System.out.print("*");
+            }
+            System.out.println (); 
+                
+            
+        }
+    }
+    public static void main(String[] args) {
+        hinh1(10);
+        
+    }
+}
